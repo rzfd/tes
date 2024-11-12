45fff9addd9a2ac82d2020a1020f5ed3d7823786
Rizki Fadillah <rizkifadillah783@gmail.com>
2024-11-12 17:44:40 +0700

    feat: init cicd project

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
+func EchoHandler(c echo.Context) error {
+	fmt.Println("Hello World")
+	return nil
+}
 
 func main() {
-	fmt.Println("asda")
+	e := echo.New()
+	e.GET("/", EchoHandler)
+	e.Logger.Fatal(e.Start(":8080"))
 }
